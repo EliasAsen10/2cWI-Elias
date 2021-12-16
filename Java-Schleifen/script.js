dab8ccf98a2c63d31fa7a7426b09b91e64e061e9
elias <you@example.com>
2021-12-16 14:50:53 +0100

    if

--- Java-Schleifen/script.js
+++ Java-Schleifen/script.js
@@ -26,7 +26,7 @@ for (let count = 0; count < 11; count++) {
 }
 console.log("Elias")
 
-for (let count = 10; count > -1; count--) {
+for (let count = 10; count > 0; count--) {
     console.log(count)
 }
 
