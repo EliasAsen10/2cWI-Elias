5feb8d1e787600d40e8fa4ceaad1ced71205c9dd
elias <you@example.com>
2022-01-20 14:20:36 +0100

    Test-2cWI-java-script

---
+++ Test-2cWI/script.js
@@ -0,0 +1,11 @@
+console.log("Beispiel1")
+for (let count = 1; count < 9; count++) {
+    console.log(count)
+}
+console.log("Das Wars!")
+
+
+console.log("Beispiel2")
+for (let number = 11; number > -4; number = number - 2) {
+    console.log(number)
+}
