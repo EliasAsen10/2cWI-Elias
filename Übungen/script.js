164210eea090ad53b74119feb5aa33b26d822f02
elias <you@example.com>
2022-01-20 13:59:12 +0100

    Text-Übung-FlexBox-Teil1

--- Übungen/script.js
+++ Übungen/script.js
@@ -1,10 +1,16 @@
 for (let count = 0; count < 11; count++) {
     console.log(count + "s")
 }
-let number = Math.floor(Math.random() * 100);
-
+let number = 88
 if (number <= 7) {
     console.log("Mini")
-} else if (number >= 7 && number <= 90) {
+} else
+if (number >= 7 && number <= 90) {
     console.log("Medium")
-} else if (number > 90) { console.log(Large) }
+} else if (number > 90) {
+    console.log(Large)
+}
+number = Math.floor(Math.random() * 100 + 10);
+console.log(number)
+
+for (let rare = 100; rare > -1; rare--) { console.log(rare + "so geht das") }
