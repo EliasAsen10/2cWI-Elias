a67f6741e77e7f5f64062e66ecfa4769a7eac916
elias <you@example.com>
2022-01-27 14:46:26 +0100

    8-Begin

---
+++ JS-08/script.js
@@ -0,0 +1,10 @@
+let random = Math.floor(Math.random() * 5 + 6);
+console.log(random);
+if (random = 10) {
+    console.log("Ten")
+} else if (radom = 9) {
+    console.log("Nine")
+} else if (random = 8) {
+    console.log("Eight")
+} else { console.log("etc") }
+//Fehler kommt immer ten
--- js-07-Operatoren/script.js
+++ js-07-Operatoren/script.js
@@ -35,8 +35,6 @@ if (Erfahrung >= 5) {
     console.log("5 Jahre Berufserfahrung")
 } else { "Master Abschluss besitzt und auf jeden Fall eine einwandfreies Leumundszeugnis" }
 
-
-
 //1. Der Fahrer darf das Motorrad fahren, wenn er 24 Jahre oder älter ist oder den Motorradführerschein besitzt aber nicht mehr in der Probezeit ist. 
 //2. Das Kind darf den Kinofilm sehen, wenn es älter als 14 ist oder einen Elternteil mit dabei hat.
 //3. Wenn ich den Kurs“ Softwarearchitekturen” besuchen möchte, muss ich den Coding Campus besucht haben oder bereits 2 Jahre Berufserfahrung vorweisen.
