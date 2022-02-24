44e87b57d72a775028526ea0fd347e80acdb00b2
elias <you@example.com>
2022-02-24 13:26:37 +0100

    Exersice-9/1 finish

--- 09-exersice/script.js
+++ 09-exersice/script.js
@@ -1,20 +1,20 @@
 let output = "";
 let isX = true
-for (let index = 1; index < 20; index++) {
+for (let index = 0; index < 20; index++) {
     if (index % 4 == 0) {
         console.log(output);
         output = ""
-        if (isX = true) {
+        if (isX == true) {
             isX = false
         } else {
-            isX = true
+            isX = true;
         }
 
     }
     if (isX == true) {
-        output = output + "x";
+        output = output + "X";
 
     } else {
         output = output + "0";
     }
-    //geht ab8 nicht mehr weiter true false fehler überschreibt nicht.
+}
