576ab9e1c63330159e5c40d12fd933a55c82c408
elias <you@example.com>
2022-03-31 14:09:19 +0200

    Exersice-11-complete

---
+++ 10-exersice/script.js
@@ -0,0 +1,24 @@
+
+function add(a,b) {
+  var result= a+b;
+  return result;
+}
+var res = add (7,3);
+console.log(res);
+
+
+function subtract(a,b) {
+  var result= a-b;
+  return result;
+}
+
+var res = subtract(10,5)
+console.log(res);
+
+
+function multiply(a,b) {
+  var result = a*b
+  return result
+}
+var res = multiply(9,3)
+console.log(res);
