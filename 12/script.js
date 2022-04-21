7545bb69fb0000aca000bcb56ed7c0dcad78aae8
elias <you@example.com>
2022-04-21 14:42:31 +0200

    calender-12

--- 12/script.js
+++ 12/script.js
@@ -1,17 +1,23 @@
-function printmonth(a) {
+
+
+function printmonth(amountOfdays) {
   var output = "";
-  for (let days = 0; days < 31; days++) {
-    
-    output = output + days;
-    if (days%7==0) {
-      console.log(output);
-      output="";
+  console.log("MO|DI|MI|DO|FR|SA|SO|");
+  for (let days = 1; days <= amountOfdays; days++) {
+    if (days < 10) {
+      output = output + " " + days + "|";
+    } else {
+      output = output + days + "|";
     }
-    week = ""
+
+    if (days % 7 == 0) {
+      console.log(output);
+      output = "";
+    }
+   
   }
+  console.log(output);
 }
 
-
-printmonth(30);
-
-
+printmonth(28);
+printmonth(31)
