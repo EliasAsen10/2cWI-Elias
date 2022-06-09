583d413ac66809111b9665dcfae34c91ed849f83
elias <you@example.com>
2022-06-09 14:30:55 +0200

    Example-3

---
+++ Example 3/script.js
@@ -0,0 +1,12 @@
+
+let output = 0;
+for (let count = 10; count > 0; count--) {
+let random = Math.random() * 100;
+let randomAsInteger = Math.round(random);
+console.log(randomAsInteger)
+output = output + randomAsInteger
+}
+
+console.log("Die Summe der 10 Zufallszahlen ist " + output);
+
+
